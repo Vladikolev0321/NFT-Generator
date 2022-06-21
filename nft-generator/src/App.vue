e2b31b56fff134ebb7f56dc57d9dddee2d22c785
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-06-21 17:30:19 +0300

    Add router and basic Home view

--- nft-generator/src/App.vue
+++ nft-generator/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ nft-generator/src/router/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+    { path: '/', redirect: '/home' },
+    {
+      path: '/home',
+      component: HomeView,
+    },
+];
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
+export default router;
