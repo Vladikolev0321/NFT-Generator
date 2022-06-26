4415b6f8d492ec748243a5187b60929a87208d34
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-06-26 11:55:53 +0300

    Add connection to metamask at the start of app

--- nft-generator/src/App.vue
+++ nft-generator/src/App.vue
@@ -5,10 +5,31 @@
 </template>
 
 <script>
+const Web3 = require('web3');
+
 export default {
   name: 'App',
   components: {
-  }
+  },
+  created() {
+    this.ethEnabled();
+  },
+  methods: {
+    async ethEnabled() {
+      if (window.ethereum) {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        this.web3 = new Web3(window.ethereum);
+        this.accounts = await this.web3.eth.getAccounts();
+        // get user account balance
+        this.web3.eth.getBalance(this.accounts[0]).then((balance) => {
+          console.log(balance);
+        });
+        return true;
+      }
+      return false;
+    },
+  },
+
 }
 </script>
 
