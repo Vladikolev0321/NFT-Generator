3ceee797f621091f3d4b88642f87e0b5d5349d74
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-06-26 11:56:26 +0300

    Add functionality for saving image to ipfs and minting the NFT

--- nft-generator/src/views/HomeView.vue
+++ nft-generator/src/views/HomeView.vue
@@ -12,10 +12,6 @@
             v-model="description.value">
         </textarea>
         <input
-            placeholder="NFT Price"
-            v-model="price.value"
-        />
-        <input
             class="upload"
             type="file"
             placeholder="NFT Image"
@@ -30,28 +26,72 @@
 </template>
 
 <script>
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import NFTAbi from '../../../contracts/build/contracts/NFT.json'
+import Web3 from 'web3'
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
 export default {
   props: {
     msg: String,
   },
   methods: {
-    submitForm() {
+    async submitForm() {
+      if (!this.name.value || !this.description.value || !this.image.value) {
+        return;
+      }
       if (!this.formIsValid) {
         return;
       }
+      let image = this.image.value;
       let name = this.name.value;
       let description = this.description.value;
-      let price = this.price.value;
       
 
       console.log('submit');
       console.log(name);
       console.log(description);
-      console.log(price);
+      console.log(image);
+
+      try {
+        const result = await client.add(JSON.stringify({image, name, description}));
+        console.log(result);
+        this.mint(result);
+      } catch(e) {
+        console.log("error uploading uri to ipfs", e);
+      }
     },
-    uploadImage(e) {
+    async mint(result) {
+      const web3 = new Web3(window.ethereum);
+      const nft = new web3.eth.Contract(
+        NFTAbi.abi,
+        '0xb904A7D08a6C3Ba86Fa4860e86F698aC6fdf3396',
+      );
+      const accounts = await web3.eth.getAccounts();
+      
+      const uri = `https://ipfs.infura.io/ipfs/${result.path}`
+      // mint nft 
+      await nft.methods.mint(uri).send({from: accounts[0]});
+      // get tokenId of new nft 
+      const id = await nft.methods.tokenCount().call();
+      
+      console.log("Token id: ", id);
+      console.log("Token uri: ", await nft.methods.tokenURI(id).call());
+    },
+    async uploadImage(e) {
       console.log('upload');
       console.log(e.target.files[0]);
+      const file = e.target.files[0];
+      if (file) {
+        try {
+          const result = await client.add(file);
+          console.log(result);
+          this.image.value = `https://ipfs.infura.io/ipfs/${result.path}`;
+        } catch(e) {
+          console.log(e);
+        }
+      }
     },
   },
   data() {
@@ -66,11 +106,6 @@ export default {
             isValid: false,
             message: ''
         }, 
-        price:{
-            value: '',
-            isValid: false,
-            message: ''
-        },
         image:{
             value: '',
             isValid: false,
